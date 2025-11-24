1c6a692d1898a2604329774593cdabe10e315b53
Asifiwe BUHENDWA <mickaelbhd@gmail.com>
2025-11-24 16:09:37 +0200

    chore(main): adding basic main menu selection feature

--- Main.java
+++ Main.java
@@ -1,5 +1,36 @@
+import menu.Menu;
+import java.util.Scanner;
+
 class Main{
 	public static void main(String[] args) {
+        Menu menu = new Menu();
+        Scanner scanner = new Scanner(System.in);
 		System.out.println("Hello World!");
+        boolean exitApp = false ;
+        char menuChoice;
+        do{
+            menu.showMainMenu();
+            menuChoice = scanner.next().trim().charAt(0);
+            switch (menuChoice){
+                case '1':
+                    System.out.println("Create Account");
+                    break;
+                case '2':
+                    System.out.println("View Accounts");
+                    break;
+                case '3':
+                    System.out.println("Process Transactions");
+                    break;
+                case '4':
+                    System.out.println("View Transaction history");
+                    break;
+                case '5':
+                    System.out.println("Exit");
+                    exitApp = true;
+                default:
+                    continue;
+            }
+
+        }while (!exitApp);
 	}
 }
